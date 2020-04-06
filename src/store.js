368873e60878a3b4f747f80e16e8b5aea9a0d097
Vincent Sandt <vincent@tetraed.re>
2020-04-06 19:14:11 +0200

    refactor: todos store

--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { writable, readable } from 'svelte/store';
-
-export const todos = writable([]);
-
-export const env = readable({ ...process.env });
---
+++ src/store/todos.js
@@ -0,0 +1,42 @@
+import { writable } from 'svelte/store';
+import xhr from '../lib/xhr';
+
+export default function createTodos() {
+  const { subscribe, set, update: updateStore } = writable([]);
+  const { APP_API_URL } = process.env;
+
+  let todos;
+  subscribe((value) => (todos = value));
+
+  return {
+    subscribe,
+
+    async fetchList() {
+      const req = await xhr(APP_API_URL + '/todos');
+      const items = await req.json();
+      updateStore(() => [...items]);
+    },
+
+    async createItem(todo) {
+      const newTodo = { ...todo };
+      await xhr(APP_API_URL + '/todos', {
+        method: 'post',
+        body: JSON.stringify(newTodo),
+      });
+    },
+
+    async updateItem(todoId, checked) {
+      let todo = todos.find((t) => t.id === todoId);
+      todo = { ...todo, checked };
+      await xhr(`${APP_API_URL}/todos/${todoId}`, {
+        method: 'put',
+        body: JSON.stringify(todo),
+      });
+    },
+
+    async getExamples() {
+      const req = await fetch(APP_API_URL + '/dummy-todos');
+      return await req.json();
+    },
+  };
+}
